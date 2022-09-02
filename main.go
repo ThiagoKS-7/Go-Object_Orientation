cf708a5707f4812a3a348c3c05e5f1b48a8ef1a6
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-09-01 21:00:05 -0300

    inital commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Thiago"
+	var numeroAgencia int = 589
+	var numeroConta int = 12345
+	var saldo float64 = 125.50
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+}
